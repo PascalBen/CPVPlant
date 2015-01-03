327b192d27bcc26f579d09a33983a7d1f5756ccc
Pascal Benoit <pascal.benoit@ise.fraunhofer.de>
2015-01-03 15:36:57 +0100

    Minor changes of configuration for scaling experiment

--- simulations/scaling/run.sh
+++ simulations/scaling/run.sh
@@ -1,15 +1,16 @@
 #!/bin/sh
 
-LOGFILE=tonight.log
+LOGFILE=run150102.log
 
 #opp_run -r 0 -u Cmdenv -c OLSR5x5 -n ../src:../../inet-2.3/examples:../../inet-2.3/src -l ../src/CPVPlant -l ../../inet-2.3/src/inet  WirelessScaling.ini >> ${LOGFILE}
 
 #opp_run -r 0 -u Cmdenv -c BATMAN5x5 -n ../src:../../inet-2.3/examples:../../inet-2.3/src -l ../src/CPVPlant -l ../../inet-2.3/src/inet WirelessScaling.ini >> ${LOGFILE}
 
-opp_run -r 0 -u Cmdenv -c DSR5x5 -n ../src:../../inet-2.3/examples:../../inet-2.3/src -l ../src/CPVPlant -l ../../inet-2.3/src/inet WirelessScaling.ini >> ${LOGFILE}
+#opp_run -r 0 -u Cmdenv -c OLSR5x5 -n ../../src:../../../inet-2.5/examples:../../../inet-2.6/src -l ../../src/CPVPlant -l ../../../inet-2.6/src/inet WirelessScaling.ini >> ${LOGFILE}
 
-opp_run -r 0 -u Cmdenv -c DYMO5x5 -n ../src:../../inet-2.3/examples:../../inet-2.3/src -l ../src/CPVPlant -l ../../inet-2.3/src/inet WirelessScaling.ini >> ${LOGFILE}
+#opp_run -r 0 -u Cmdenv -c DYMO5x5 -n ../src:../../inet-2.3/examples:../../inet-2.3/src -l ../src/CPVPlant -l ../../inet-2.3/src/inet WirelessScaling.ini >> ${LOGFILE}
 
+opp_run -r 0 -u Cmdenv -c OLSR5x5 -n ../../src:../../../inet-2.5/examples:../../../inet-2.5/src -l ../../src/CPVPlant -l ../../../inet-2.5/src/inet WirelessScaling.ini >> ${LOGFILE}
 
 
 
